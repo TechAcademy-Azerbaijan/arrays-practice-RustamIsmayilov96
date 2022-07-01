85be74fdc55e3ce12d7a042c8400ab9e0dc21d79
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-01 14:04:51 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -3,7 +3,17 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+    var input = result.input; // "5,2 4 7 12 9"
+    var input_data = input.split(','); // ['5', '2 4 7 12 9']
+    let arr = []
+    for(let i = 0; i<input_data.length; i++){
+    if(!arr.includes(input_data[i])){
+        arr.push(input_data[i])
+    }
+    }console.log(arr);
 });
